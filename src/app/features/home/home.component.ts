0255e9da04d7e1a61962b70aa112670f4a3369ac
Dominique Börner <69742611+mukosoft@users.noreply.github.com>
2022-06-15 18:26:26 +0200

    remove unused stuff

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { TwitchIntegration } from '../../../environments/twitch-integration';
 import { TwitchService } from '../../core/services/twitch-service/twitch.service';
 import { TwitchResponse } from '../../core/models/twitch-response';
 
