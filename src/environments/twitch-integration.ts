a8586d8912de7191796819b0d380a96675bde6d4
Dominique Börner <dominique@mukosoft.de>
2023-01-28 22:48:42 +0100

    Update access token

--- src/environments/twitch-integration.ts
+++ src/environments/twitch-integration.ts
@@ -1,4 +1,4 @@
 export const TwitchIntegration = {
   CLIENT_ID: 'oxterfmvryowcyurvzmz3x1vs1j93z',
-  ACCESS_TOKEN: 'ph0am02pmz1ezqruvvl5efflsf7101',
+  ACCESS_TOKEN: '6amcrn2qzroxqu5fkd3bn7p2q7ewuw',
 };
